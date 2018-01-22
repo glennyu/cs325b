0c1a47a74c658d99639593e7b5215e9dade6e8b2
Eric Xu <ericxu0@stanford.edu>
2018-01-21 22:00:17 -0800

    added random sampling to tweets

--- data_utils/tweet_processing.py
+++ data_utils/tweet_processing.py
@@ -1,33 +1,44 @@
 import collections
 import csv
+import html
 import nltk
 from nltk.tokenize import word_tokenize
+import numpy as np
 import string
 import os
 
-PATH = '/mnt/mounted_bucket/'
+PATH = './'#'/mnt/mounted_bucket/'
+SAMPLING_RATE = 0.1
 
 def parse_files():
     tweetWordLen = collections.defaultdict(int)
     for filename in os.listdir(PATH):
         if filename.endswith(".csv") and filename == 'tweets_201401.csv':
+            print("reading", filename)
+            numEntries = len(open(PATH + filename).readlines()) - 1
+            numSamples = int(SAMPLING_RATE*numEntries)
+            print("sampling", numSamples)
+            indices = set(np.random.choice(numEntries, numSamples, replace=False))
             with open(PATH + filename) as csvfile:
                 reader = csv.DictReader(csvfile)
-                numRowsRead = 0
+                numEntriesRead, index = 0, 0
                 for row in reader:
-                    tokens = word_tokenize(row["tweet"])
-                    tokens = [w.lower() for w in tokens]
-                    table = str.maketrans('', '', string.punctuation)
-                    stripped = [w.translate(table) for w in tokens]
-                    words = [word for word in stripped if word.isalpha()]
-                    # filter out stop words
-                    #from nltk.corpus import stopwords
-                    #stop_words = set(stopwords.words('english'))
-                    #words = [w for w in words if not w in stop_words]
-                    tweetWordLen[len(words)] += 1
-                    numRowsRead += 1
-                    if numRowsRead % 1000000 == 0:
-                        print(numRowsRead)
+                    if index in indices:
+                        tokens = word_tokenize(html.unescape(row["tweet"]))
+                        tokens = [w.lower() for w in tokens]
+                        words = [word for word in tokens if word.isalpha()]
+                        # filter out stop words
+                        # from nltk.corpus import stopwords
+                        # stop_words = set(stopwords.words('english'))
+                        # words = [w for w in words if not w in stop_words]
+                        tweetWordLen[len(words)] += 1
+                        numEntriesRead += 1
+                        if numEntriesRead % 100000 == 0:
+                            print(numEntriesRead)
+                            print(row["tweet"])
+                            print(words)
+                    index += 1
+
     print(tweetWordLen)
 
 def main():
