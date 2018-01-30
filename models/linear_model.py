65c33290095ba3c76115eaeb19db8b05b32af849
Eric Xu <ericxu0@stanford.edu>
2018-01-30 00:12:03 -0800

    graphed lin reg trends

--- models/linear_model.py
+++ models/linear_model.py
@@ -10,7 +10,7 @@ from nltk.corpus import stopwords
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.tokenize import word_tokenize
 import numpy as np
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
 import string
 import os
 import sys
@@ -75,21 +75,24 @@ def read_features():
     f_cnt.close()
     return sentiment_feat, food_cnt
 
-def lin_reg(sentiment_feat, food_cnt, prices):
+def lin_reg(food_idx, sentiment_feat, food_cnt, prices):
     feat = np.hstack((sentiment_feat, np.expand_dims(food_cnt, axis=1)))
     train_feat, test_feat = feat[:24], feat[24:]
     train_prices, test_prices = prices[:24], prices[24:]
 
-    reg = LinearRegression()
+    reg = Ridge()
     reg.fit(train_feat, train_prices)
+    print(food_to_predict[food_idx])
     #print(reg.coef_)
-    #print("score: %f" % reg.score(train_feat, train_prices))
+    print("score: %f" % reg.score(train_feat, train_prices))
     pred = np.squeeze(reg.predict(test_feat))
     #print("predictions:")
     #print(pred)
     #print("actual:")
     #print(np.squeeze(test_prices))
     print("MAPE: %f" % mape(np.squeeze(test_prices), pred)) 
+    plot_price_trend(2*food_idx, np.squeeze(reg.predict(train_feat)), np.squeeze(train_prices), food_to_predict[food_idx] + "_train_sentiment")
+    plot_price_trend(2*food_idx + 1, pred, np.squeeze(test_prices), food_to_predict[food_idx] + "_test_sentiment")
 
 def main():
     #get_features()
@@ -97,8 +100,7 @@ def main():
     prices = get_prices("Delhi")
     for i in range(len(food_to_predict)):
         idx = food_to_index[i]
-        print(food_to_predict[i])
-        lin_reg(np.squeeze(sentiment_feat[idx:(idx + 1)]), np.squeeze(food_cnt[idx:(idx + 1)]), prices[i:(i + 1)].T)
+        lin_reg(i, sentiment_feat[idx], np.squeeze(food_cnt[idx:(idx + 1)]), prices[i:(i + 1)].T)
 
 if __name__ == "__main__":
     main()
