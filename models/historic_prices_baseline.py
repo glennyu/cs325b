cc36ec1c31633ab06a04840d2dda5b0e7b2aaf2b
chenyaoy <icyu1027@gmail.com>
2018-03-19 21:03:55 -0700

    Added plotting confusion matrices for historic prices baseline

--- models/historic_prices_baseline.py
+++ models/historic_prices_baseline.py
@@ -1,10 +1,15 @@
 from collections import defaultdict
 import csv
 import numpy as np
+import itertools
 from sklearn.linear_model import RidgeClassifier as RC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import accuracy_score
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 
 DIR = '../data/'
 NUM_MONTHS = 38
@@ -75,6 +80,40 @@ def split_data(city_to_prices):
     print 'decrease: %d, no change: %d, increase: %d' % (decrease, no_change, increase)
     return np.array(X), np.array(y_dir), np.array(y_spike)
 
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center", fontsize=16,
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig(title + '.png')
+
+def get_conf_matrix(pred, labels, classes, title):
+    plt.figure()
+    conf_matrix = np.zeros((len(classes), len(classes)), dtype=np.float32)
+    for i in range(labels.shape[0]):
+        conf_matrix[labels[i]][pred[i]] += 1
+    plot_confusion_matrix(conf_matrix,
+                          classes=classes,
+                          normalize=True,
+                          title=title)
+
+
 def random_forest_classifier(features, target):
     """
     To train the random forest classifier with features and target data
@@ -99,15 +138,27 @@ def gradient_boosting_classifier(features, target):
 
 # Output training and validation accuracies using 
 # Ridge Classifier
-def train_model(X, y):
+def train_model(X, y, predictSpike):
     X_train, y_train = X[:len(X)/2], y[:len(y)/2]
     X_val, y_val = X[len(X)/2:], y[len(y)/2:]
 
     if model == "Random Forest":
-        trained_model = random_forest_classifier(X_train, y_train)
-        predictions = trained_model.predict(X_val)
-        mean_train_acc = accuracy_score(y_train, trained_model.predict(X_train))
+        rf = RandomForestClassifier()
+        rf.fit(X_train, y_train)
+        predictions = rf.predict(X_val)
+        mean_train_acc = accuracy_score(y_train, rf.predict(X_train))
         mean_val_acc = accuracy_score(y_val, predictions)
+        if predictSpike:
+            get_conf_matrix(rf.predict(X_train), y_train, 
+                    ['no spike', 'spike'], 'Historic Prices Random Forest Price Spike Train')
+            get_conf_matrix(rf.predict(X_val), y_val, 
+                    ['no spike', 'spike'], 'Historic Prices Random Forest Price Spike Validation')
+        else:
+            get_conf_matrix(rf.predict(X_train), y_train, 
+                    ['decrease', 'no change', 'increase'], 'Historic Prices Random Forest Price Direction Train')
+            get_conf_matrix(rf.predict(X_val), y_val, 
+                    ['decrease', 'no change', 'increase'], 'Historic Prices Random Forest Price Direction Validation')
+
     elif model == "Ridge Classifier":
         clf = RC()
         clf.fit(X_train, y_train)
@@ -126,10 +177,10 @@ def main():
     city_to_prices = get_prices()
     X, y_dir, y_spike = split_data(city_to_prices)
     print 'Predicting price direction...'
-    train_model(X, y_dir)
+    train_model(X, y_dir, False)
     print 'Predicting price spike...'
-    train_model(X, y_spike)
-
+    train_model(X, y_spike, True)
+    
 
 if __name__ == '__main__':
     main()
